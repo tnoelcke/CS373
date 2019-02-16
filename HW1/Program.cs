45dff3e1952519cb1d1a55f8b4287772f11a3b14
Thomas <noelcket@oregonstate.edu>
2019-02-15 20:51:56 -0800

    Added scaffolding for project one

---
+++ HW1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool quit = false;
+            string input;
+            while(!quit){
+                Console.Write("$ ");
+                input = Console.ReadLine();
+                if(input.ToLower().StartsWith("q")){
+                    quit = true;
+                } else {
+                    processInput(input);
+                }
+            }
+
+        }
+
+        // Handles choosing what function to run
+        static void processInput(string input){
+            string split = input.split(" ");
+            if()
+        }
+    }
+}

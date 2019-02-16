e335efd47fc67050684f0a25db04d4b11afb06f4
Thomas <noelcket@oregonstate.edu>
2019-02-15 21:14:04 -0800

    finished scafolding for HW1

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -22,8 +22,29 @@ namespace HW1
 
         // Handles choosing what function to run
         static void processInput(string input){
-            string split = input.split(" ");
-            if()
+            string[] split = input.Split(' ');
+            switch(split[0]){
+                case "help":
+                    printCommands();
+                    break;
+                default:
+                    foreach(string item in split){
+                        Console.Write("$ " + item + " ");
+                    }
+                    Console.Write("is not a valid command. Type help for a list of valid commands.");
+                    Console.WriteLine();
+                    break;
+            }
+        }
+
+        static void printCommands(){
+            Console.WriteLine("Below is a valid list of commands that can be run in the program:");
+            Console.WriteLine("p -                      Lists out all the processes");
+            Console.WriteLine("p <processId>            Lists out all of the running threads with in a process");
+            Console.WriteLine("pm <processId>           Lists out all the modules loaded in a process");
+            Console.WriteLine("page <processId>         show all executable pages in a process");
+            Console.WriteLine("mr <processId> <#bytes>  read #bytes form a processes memory starting at the lowest address (out put in hex)");
+            Console.WriteLine("q                        quit");            
         }
     }
 }

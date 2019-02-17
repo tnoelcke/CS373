7fce40d78f268e07c0a30e17c4b572af6145f62a
Thomas <noelcket@oregonstate.edu>
2019-02-16 20:57:53 -0800

    added list page feature

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,10 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 
 namespace HW1
 {
     class Program
     {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool ReadProcessMemory(
+            IntPtr hProcess,
+            IntPtr lpBaseAddress,
+            [Out] byte[] lpBuffer,
+            int dwSize,
+            out IntPtr lpNumberOfBytesRead);
+
+        public enum AllocationProtect : uint 
+        { 
+            PAGE_EXECUTE = 0x00000010, 
+            PAGE_EXECUTE_READ = 0x00000020, 
+            PAGE_EXECUTE_READWRITE = 0x00000040, 
+            PAGE_EXECUTE_WRITECOPY = 0x00000080, 
+            PAGE_NOACCESS = 0x00000001, 
+            PAGE_READONLY = 0x00000002, 
+            PAGE_READWRITE = 0x00000004, 
+            PAGE_WRITECOPY = 0x00000008, 
+            PAGE_GUARD = 0x00000100, 
+            PAGE_NOCACHE = 0x00000200, 
+            PAGE_WRITECOMBINE = 0x00000400 
+        }
+
+        private const long MAX_ADDRESS = 0x7fffffff;
+
+        [StructLayout(LayoutKind.Sequential)] 
+        public struct MEMORY_BASIC_INFORMATION 
+        { 
+            public IntPtr BaseAddress; 
+            public IntPtr AllocationBase; 
+            public uint AllocationProtect; 
+            public IntPtr RegionSize; 
+            public uint State; 
+            public uint Protect; 
+            public uint Type; 
+        }
+
+        [DllImport("kernel32.dll")]
+        static extern int VirtualQueryEx(IntPtr hProcess, IntPtr lpAddress, out MEMORY_BASIC_INFORMATION lpBuffer, uint dwLength);
+
+
         static void Main(string[] args)
         {
             bool quit = false;
@@ -56,7 +100,7 @@ namespace HW1
             Console.WriteLine("p <processId>            Lists out all of the running threads with in a process");
             Console.WriteLine("pm <processId>           Lists out all the modules loaded in a process");
             Console.WriteLine("page <processId>         show all executable pages in a process");
-            Console.WriteLine("mr <processId> <#bytes>  read #bytes form a processes memory starting at the lowest address (out put in hex)");
+            Console.WriteLine("mr <processId>           read #bytes form a processes memory starting at the lowest address (out put in hex)");
             Console.WriteLine("q                        quit");            
         }
 
@@ -108,7 +152,7 @@ namespace HW1
                 if(Int32.TryParse(args[1], out processId)){
                     listModules(processId);
                 } else {
-                    Console.WriteLine("p command requires a valid process id, " + args[1] + "is not a valid process id");
+                    Console.WriteLine("pm command requires a valid process id, " + args[1] + "is not a valid process id");
                 }
             }
         }
@@ -129,11 +173,62 @@ namespace HW1
         }
 
         static void handlePages(string[] args){
-
+            if(args.Length <= 1){
+                Console.WriteLine("The page command requires an event Id");
+                Console.WriteLine("page <processId>");
+            } else {
+                int processId = 0;
+                if(Int32.TryParse(args[1], out processId)){
+                    listPages(processId);
+                } else {
+                    Console.WriteLine("page command requires a valid process id, " + args[1] + "is not a valid process id");
+                }
+            }
         }
 
         static void handleMemoryRead(string[] args){
+            if(args.Length <= 1){
+                Console.WriteLine("The mr command requires an event Id");
+                Console.WriteLine("mr <processId>");
+            } else {
+                int processId = 0;
+                if(Int32.TryParse(args[1], out processId)){
+                    listModules(processId);
+                } else {
+                    Console.WriteLine("mr command requires a valid process id, " + args[1] + "is not a valid process id");
+                }
+            }
+        }
 
+        static void readMenu () {
+
+        }
+
+        static void listPages(int processId){
+            long address = 0;
+            Process currentProcess;
+            try{
+            currentProcess = Process.GetProcessById(processId);
+            IntPtr hProcess = currentProcess.Handle;
+            Console.WriteLine("-------------------------------------------------------------------------------------------------");
+            Console.WriteLine("| Base Address \t|\t Region Size \t|\t State |");
+            while(address < MAX_ADDRESS) 
+            {
+                MEMORY_BASIC_INFORMATION memInfo;
+                int result = VirtualQueryEx(hProcess, (IntPtr) address, out memInfo, (uint)Marshal.SizeOf(typeof(MEMORY_BASIC_INFORMATION)));
+                if(memInfo.AllocationProtect == (uint)AllocationProtect.PAGE_EXECUTE ||
+                    memInfo.AllocationProtect == (uint)AllocationProtect.PAGE_EXECUTE_READ ||
+                    memInfo.AllocationProtect == (uint)AllocationProtect.PAGE_EXECUTE_READWRITE ||
+                    memInfo.AllocationProtect == (uint)AllocationProtect.PAGE_EXECUTE_WRITECOPY){
+                        Console.WriteLine("| " + (uint)memInfo.BaseAddress + "\t|\t" + memInfo.RegionSize + "\t|\t" + memInfo.State + " |");
+                }
+                address = (long) memInfo.BaseAddress + (long)memInfo.RegionSize;
+            }
+            } catch (Exception e) {
+                Console.WriteLine("Could Not find Process Id " + processId);
+                Console.WriteLine("Error: " + e.ToString());
+            }
+            Console.WriteLine("-------------------------------------------------------------------------------------------------");
         }
     }
 }
